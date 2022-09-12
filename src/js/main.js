6d198553ce586cd2dd410c6beae8f6358465042b
Miguel <miguelaf44@gmail.com>
2022-09-12 11:42:59 -0500

    Added Reports Grid & Fixed Main JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,22 @@
 var reportsWidget = {
     options: {
         containerSelector: '.reports',
-        template: (
-            '{{#.}}' +
-                '<article class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
-                    '<footer class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
-        )
+        template: `
+            {{#.}} 
+                <article class="reports_item"> 
+                    <a href="{{cover}}" target="_blank"> 
+                        <img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>
+                    </a> 
+                    <footer class="reports_docs"> 
+                        {{#documents}} 
+                            <h3 class="reports_title"> 
+                                <a href="{{url}}" target="_blank">{{title}} <span>  ({{file_size}} <div> {{file_type}}) </div> </span> </a> 
+                            </h3> 
+                        {{/documents}} 
+                    </footer> 
+                </article> 
+            {{/.}}
+        `
     },
 
     init: function() {
